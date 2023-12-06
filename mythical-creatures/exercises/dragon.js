02909746d23c809404d93a76a155e0fc5be50efd
Tayla Phillips <richardstayla@gmail.com>
2023-12-05 22:28:01 -0600

    Pass tests 9-10

--- mythical-creatures/exercises/dragon.js
+++ mythical-creatures/exercises/dragon.js
@@ -1,20 +1,31 @@
 function createDragon(name, rider, temperment) {
-return {
-  name: name,
-  rider: rider,
-  temperment: temperment,
-  timesEaten: 0
-}
+  return {
+    name, //syntactic sugar
+    rider,
+    temperment,
+    timesEaten: 0,
+    hungry: true
+  }
 };
 
 function greetRider(dragon) {
-return `Hi, ${dragon.rider}!`
+  return `Hi, ${dragon.rider}!`
 };
 
+function eat(dragon) {
+  // console.log('dragon fed +1: ', dragon.timesEaten = dragon.timesEaten + 1);
+  // return dragon.timesEaten = (dragon.timesEaten + 1)
+  console.log('dragon times eaten: ', dragon.timesEaten)
+  dragon.timesEaten ++
+  if (dragon.timesEaten >= 3) {
+    dragon.hungry = false
+  }
+  return dragon
+};
 
 module.exports = {
   createDragon, 
   greetRider, 
-  // eat, 
+  eat, 
   // findFireBreathers
 }
