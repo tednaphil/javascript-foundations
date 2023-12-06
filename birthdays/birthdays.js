bb026428af8f71beeac4fa5c79c13b2c61fdc6b7
Tayla Phillips <richardstayla@gmail.com>
2023-12-06 15:39:38 -0600

    Pass tests 1-2

--- birthdays/birthdays.js
+++ birthdays/birthdays.js
@@ -1,8 +1,20 @@
+function createBirthday(name, month, day) {
+    return {
+        name,
+        month,
+        day
+    }
+};
 
-
+function celebrateBirthday(birthdayArray) {
+    console.log('birthday object.month: ', birthdayArray.month)
+    var birthdate = `${birthdayArray.month}/${birthdayArray.day}`
+    console.log('birthdate: ', birthdate)
+    return `Today is ${birthdate}! Happy birthday, ${birthdayArray.name}!`
+};
 
 module.exports = {
-    // createBirthday,
-    // celebrateBirthday
+    createBirthday,
+    celebrateBirthday
     // countBirthdays
   };
