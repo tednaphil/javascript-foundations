dffec609d179bff4a582b0f1f97876210a8337b7
Tayla Phillips <richardstayla@gmail.com>
2023-12-05 21:37:32 -0600

    Pass tests 1-6

--- mythical-creatures/exercises/dragon.js
+++ mythical-creatures/exercises/dragon.js
@@ -1,7 +1,13 @@
-
+function createDragon(name, rider, temperment) {
+return {
+  name: name,
+  rider: rider,
+  temperment: temperment
+}
+};
 
 module.exports = {
-  // createDragon, 
+  createDragon, 
   // greetRider, 
   // eat, 
   // findFireBreathers
