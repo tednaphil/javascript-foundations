98859f33982fc38a5b706d0c81040ecd0df6bbc6
Tayla Phillips <richardstayla@gmail.com>
2023-12-08 10:57:47 -0600

    Pass tests 1-6

--- tacoStand/tacoStand.js
+++ tacoStand/tacoStand.js
@@ -1,2 +1,37 @@
+// create function called create ingredient - export it
+// two parameters - ingredient, price
+// return an object with a name and price property
+//name will be the first argument and price will be the second arg
+function createIngredient(ingredient, price) {
+    return {
+        name: ingredient || 'unknown',
+        price: price || 0.00
+    }
 
-module.exports = {}
+    
+    
+    // if (ingredient === undefined || price === undefined) {
+    //     return {
+    //         name: 'unknown',
+    //         price: 0.00
+    //     }
+    // } else
+    //     return {
+    //      name: ingredient,
+    //      price,
+    //  }
+};
+
+function createTaco(tacoName, ingredientsArray) {
+    return {
+        name: tacoName || 'custom',
+        ingredients: ingredientsArray || []
+    }
+};
+
+module.exports = {
+    createIngredient,
+    createTaco,
+
+
+}
