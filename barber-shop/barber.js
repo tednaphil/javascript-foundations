f5e9561691959529e7a3d3dda72a6bcaf772d50b
Tayla Phillips <richardstayla@gmail.com>
2023-12-11 21:10:50 -0600

    Pass tests 1-4

--- barber-shop/barber.js
+++ barber-shop/barber.js
@@ -1,3 +1,20 @@
 
+function createBarber(name, earnings, haircuts) {
+ return {
+    name,
+    earnings: earnings || 0,
+    haircuts: haircuts || []
+ }
+};
 
-module.exports = {  };
+function giveCompliment(haircut) {
+ return `This ${haircut.style} looks great!`
+}
+
+
+module.exports = {
+    createBarber,
+    giveCompliment,
+    // cutHair,
+    // listStyles
+  };
