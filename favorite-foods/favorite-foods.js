6c60ae3d63c93cd48424bbf62ada1bdef27a7af5
Tayla Phillips <richardstayla@gmail.com>
2023-12-12 21:08:45 -0600

    Pass tests 1-8

--- favorite-foods/favorite-foods.js
+++ favorite-foods/favorite-foods.js
@@ -1,4 +1,38 @@
+function createFavoriteFood(foodObject) {
+    // console.log(foodObject.dish)
+    // console.log(foodObject.ingredients)
+    return {
+        name: foodObject.dish,
+        ingredients: foodObject.ingredients,
+        isSpicy: foodObject.isSpicy,
+        timesOrdered: 0
+    }
+
+};
+
+function commentOnSpiciness(dishObject) {
+//    console.log(dishObject.isSpicy, 'is it spicy?')
+    if (dishObject.isSpicy === true) {
+        return `Wow, this ${dishObject.name} is so spicy!`
+    } else {
+        return `Phew, this ${dishObject.name} is not very spicy.`
+    }
+    
+    //if the item is spicy say "wow..."
+    //if the item is not spicy say "phew..."
+};
+
+function orderFood(dishObject) {
+    dishObject.timesOrdered += 1
+    // console.log(dishObject.timesOrdered)
+    // console.log('dishObject post increment: ', dishObject)
+    return dishObject
+}
 
 
-
-module.exports = { };
+module.exports = {
+    createFavoriteFood,
+    commentOnSpiciness,
+    orderFood,
+    // createShoppingList
+ };
