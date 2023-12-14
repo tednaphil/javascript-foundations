712baa109e65d60e1628a8e2439592ed813f44b2
Tayla Phillips <richardstayla@gmail.com>
2023-12-13 21:45:18 -0600

    Unskip all tests

--- favorite-foods/favorite-foods-test.js
+++ favorite-foods/favorite-foods-test.js
@@ -81,7 +81,7 @@ describe('favorite foods', function () {
    })
 
    // spicy 🌶️
-   it.skip('should be able to make a list of all ingredients needed for multiple dishes', function () {
+   it('should be able to make a list of all ingredients needed for multiple dishes', function () {
       var pizza = createFavoriteFood({ dish: 'Pizza', ingredients: ['Tomato Sauce', 'Cheese', 'Pepperoni', 'Mushrooms'], isSpicy: false });
       var smoothie = createFavoriteFood({ dish: 'Smoothie', ingredients: ['Banana', 'Strawberry', 'Blueberry', 'Milk', 'Honey'], isSpicy: false })
 
