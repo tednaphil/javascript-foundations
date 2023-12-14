76d273129405fef64af2ba3066d7573efce37144
Tayla Phillips <richardstayla@gmail.com>
2023-12-14 11:00:07 -0600

    Pass tests 3-6

--- crafting/crafts.js
+++ crafting/crafts.js
@@ -13,12 +13,48 @@ function calculateMaterialCost(materialObject, num) {
   return `${num} ${materialObject.unit}s of ${materialObject.name} costs $${cost}.`
 };
 
+function createSupplyCloset(supplyArray) {
+  // console.log(supplyArray)
+  // console.log(supplyArray[0].name)
+  if (!supplyArray) {
+    return {
+      supplies: []
+    }
+  } else {
+  var supplyList = [];
+  for (var i = 0; i < supplyArray.length; i++) {
+    supplyList.push(supplyArray[i].name)
+  }
+  // console.log('supplyArray: ', supplyList)
+  return {
+    supplies: supplyList
+  }
+}
+};
+
+// input: array of objects
+// output: object with supplies as one key that is assigned to the value of an array of strings
+
+function addSupply(closetObject, materialObject) {
+  // console.log('closet supplies:', closetObject.supplies);
+  // console.log('material name: ', materialObject.name)
+  if (closetObject.supplies.includes(materialObject.name)) {
+    return `You already have ${materialObject.name} in your closet!`
+  } else {
+  closetObject.supplies.push(materialObject.name)
+  // console.log('updated closet list:', closetObject.supplies)
+  return closetObject.supplies
+  }
+};
+
+// input: two objects
+// output: array of threads
 
 module.exports = {
   createMaterial,
   calculateMaterialCost,
-  // createSupplyCloset,
-  // addSupply,
+  createSupplyCloset,
+  addSupply,
   // createNewProject,
   // compareMaterials
 }
