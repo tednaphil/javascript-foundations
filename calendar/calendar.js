50029a818baf6347784628e4d8f04be65f417ab2
Tayla Phillips <richardstayla@gmail.com>
2023-12-17 11:10:40 -0600

    Pass all tests

--- calendar/calendar.js
+++ calendar/calendar.js
@@ -1,3 +1,39 @@
+function createEvent(title, month, day) {
+    if (day > 31 || day < 1) {
+        return `Error: ${day} is not a valid day`
+    } else {
+        return {
+         title,
+         month,
+         day
+        }
+     }
+}
 
+function createCalendar(owner, eventsArray) {
+    return {
+        owner,
+        events: eventsArray
+    }
+}
 
-module.exports = {  };
+function reportMonthlyEvents(calendarObject, month) {
+    var monthlyEvents = []
+    calendarObject.events.forEach((object) => {
+        if (month === object.month) {
+            monthlyEvents.push(object)
+        }
+    })
+    // console.log(calendarObject.events)
+    return monthlyEvents
+}
+
+// output: array of objects
+//declare a variable to store the monthly events
+// if the month is included in the calendar object, push to monthly events array
+
+module.exports = { 
+    createEvent,
+    createCalendar,
+    reportMonthlyEvents
+ };
