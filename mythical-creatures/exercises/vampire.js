3deb5b97bc7b51fff98b0593d00c706e5ff7ea04
Tayla Phillips <richardstayla@gmail.com>
2023-12-17 10:41:28 -0600

    Remove console logs

--- mythical-creatures/exercises/vampire.js
+++ mythical-creatures/exercises/vampire.js
@@ -10,7 +10,6 @@ function createVampire(name, pet) {
 function encounterDeliciousVictim(vampire) {
   if (vampire.thirsty) {
     return `I WANT TO SUCK YOUR BLOOD!`
-    // console.log('vampire.thirsty value: ', vampire.thirsty)
   } else {
     return `No thanks, I am too full.`
   }
@@ -22,7 +21,6 @@ function drink(vampire) {
   if (vampire.ouncesDrank >= 50) {
     vampire.thirsty = false
   }
-  // console.log(vampire.ouncesDrank)
   return vampire
 };
 
@@ -41,11 +39,9 @@ function findBatLovers(vampireArray) {
       batLovers.push(object.name)
     }
   })
-  console.log('batLovers array: ', batLovers)
   return batLovers
 }
-// input: array of objects
-// output: array of strings
+
 module.exports = {
   createVampire, 
   drink, 
