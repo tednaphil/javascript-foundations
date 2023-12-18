334d3e76aadef4bb0cb771276dedbf758e131e83
Tayla Phillips <richardstayla@gmail.com>
2023-12-18 10:30:37 -0600

    Remove console logs and pseudocode

--- crafting/crafts.js
+++ crafting/crafts.js
@@ -7,15 +7,11 @@ function createMaterial(name, unit, costPerUnit) {
 };
 
 function calculateMaterialCost(materialObject, num) {
-  // console.log('arguemnt cpu: ', materialObject.costPerUnit)
   var cost = num * materialObject.costPerUnit
-  // console.log('calculated cost: ', cost)
   return `${num} ${materialObject.unit}s of ${materialObject.name} costs $${cost}.`
 };
 
 function createSupplyCloset(supplyArray) {
-  // console.log(supplyArray)
-  // console.log(supplyArray[0].name)
   if (!supplyArray) {
     return {
       supplies: []
@@ -25,30 +21,22 @@ function createSupplyCloset(supplyArray) {
   for (var i = 0; i < supplyArray.length; i++) {
     supplyList.push(supplyArray[i].name)
   }
-  // console.log('supplyArray: ', supplyList)
   return {
     supplies: supplyList
   }
 }
 };
 
-// input: array of objects
-// output: object with supplies as one key that is assigned to the value of an array of strings
 
 function addSupply(closetObject, materialObject) {
-  // console.log('closet supplies:', closetObject.supplies);
-  // console.log('material name: ', materialObject.name)
   if (closetObject.supplies.includes(materialObject.name)) {
     return `You already have ${materialObject.name} in your closet!`
   } else {
   closetObject.supplies.push(materialObject.name)
-  // console.log('updated closet list:', closetObject.supplies)
   return closetObject.supplies
   }
 };
 
-// input: two objects
-// output: array of threads
 
 function createNewProject(materialsArray, status) {
   return {
@@ -57,28 +45,16 @@ function createNewProject(materialsArray, status) {
   }
 }
 
-// input: array of objects, and a string
-//output: object with two known keys (materialsNeeded and status)
-
 function compareMaterials(projectObject, suppliesObject) {
-  // console.log(suppliesObject)
-  // console.log('supplies list: ', suppliesObject.supplies)
-  // console.log('materialsNeeded: ', projectObject.materialsNeeded)
   var materialsNeeded = []
   projectObject.materialsNeeded.forEach((object) => {materialsNeeded.push(object.name)})
-  // console.log('materialsNeeded array: ', materialsNeeded)
-  // console.log('supplies object array: ', suppliesObject.supplies)
-  // console.log('do these arrays match?', materialsNeeded === suppliesObject.supplies)
-  // console.log(suppliesObject.supplies.toString())
       if (materialsNeeded.toString() === suppliesObject.supplies.toString()) {
         return `Yay! You can start this project!`
       } else {
         return `Oh no! You need to go shopping before you can start this project!`
       }
 }
-// input: two arguments, projectObject and suppliesObject(has a property
-//that's an array of objects)
-// output: string
+
 
 module.exports = {
   createMaterial,
