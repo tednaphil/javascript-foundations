5211d1e69d1146b0371b624382bf8dbb25b8b937
Tayla Phillips <richardstayla@gmail.com>
2023-12-19 10:47:49 -0600

    Pass remaining tests

--- tacoStand/tacoStand.js
+++ tacoStand/tacoStand.js
@@ -29,9 +29,29 @@ function createTaco(tacoName, ingredientsArray) {
     }
 };
 
+function addIngredientToTaco(tacoObject, ingredient) {
+    // console.log('ingredients array: ',tacoObject.ingredients)
+    tacoObject.ingredients.push(ingredient)
+    // console.log('new ingredients array: ', tacoObject.ingredients)
+    return tacoObject
+
+};
+
+function calculatePrice(tacoObject) {
+    // console.log('ingredient price: ', tacoObject.ingredients[0].price)
+    var cost = 0;
+    tacoObject.ingredients.forEach((object) => (cost += object.price))
+    // console.log('cost: ', cost)
+    return cost
+
+}
+
+// input taco object
+// output: number
+
 module.exports = {
     createIngredient,
     createTaco,
-
-
+    addIngredientToTaco,
+    calculatePrice
 }
