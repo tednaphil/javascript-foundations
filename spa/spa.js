00494452308ea961694ff3d4f96c46392dc28d37
Tayla Phillips <richardstayla@gmail.com>
2023-12-28 08:19:43 -0600

    Pass tests 1-4

--- spa/spa.js
+++ spa/spa.js
@@ -1,8 +1,39 @@
 
+function createCustomer(name, bill, bookingsArray) {
+  return {
+    name,
+    bill,
+    bookings: bookingsArray || []
+  }
+};
+
+function greeting(customerObject) {
+  if (customerObject.bookings.length > 0) {
+    return `${customerObject.name}! Welcome back to Happy Spa`
+  } else {
+    return `${customerObject.name}! Welcome to Happy Spa`
+  }
+};
+
+function createService(serviceName, cost) {
+  if (serviceName) {
+    return {
+      name: serviceName,
+      cost,
+    }
+  } else {
+    return `Please provide service name and cost.`
+  }
+};
+
+function bookServices(customerObject, serviceObject) {
+  
+}
+
 module.exports = { 
-  // createCustomer, 
-  // greeting, 
-  // createService,
-  // bookServices, 
+  createCustomer, 
+  greeting, 
+  createService,
+  bookServices, 
   // applyGiftCard 
 }
