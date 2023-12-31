b81aaa1e8733abcc117dad3ce086318e0ce28960
Tayla Phillips <richardstayla@gmail.com>
2023-12-30 21:56:37 -0600

    Pass all tests

--- library/library.js
+++ library/library.js
@@ -1,3 +1,27 @@
+function issueCard(name, age) {
+    return {
+        name,
+        age,
+        numBooksCheckedOut: 0,
+        isChild: age < 12 ? true : false
+    }
+}
 
+function searchByAuthor(collectionArray, author) {
+    var results = [];
+    collectionArray.forEach((object) => {
+        if (object.author === author) {
+            results.push(object)
+        }
+    })
+    if (results.length === 0) {
+        return `No book found for search criteria`
+    } else {
+        return results
+    }
+}
 
-module.exports = { };
+module.exports = {
+    issueCard,
+    searchByAuthor,
+ };
