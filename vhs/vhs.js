04d126ac04d52f6ffd7b99365248a6043d474533
Tayla Phillips <richardstayla@gmail.com>
2023-12-30 21:28:32 -0600

    Pass all tests

--- vhs/vhs.js
+++ vhs/vhs.js
@@ -1,3 +1,39 @@
+function createTape(title, readyToPlay) {
+    return {
+        title: title,
+        readyToPlay: readyToPlay || false,
+    }
+}
 
+function reset(vhsObject) {
+    vhsObject.readyToPlay = true
+    return vhsObject
+}
 
-module.exports = {  }
+function createCollection(vhs1, vhs2, vhs3) {
+    var newCollection = []
+    if (!vhs1 || !vhs2 || !vhs3) {
+        return `Your collection is empty.`
+    // vhsObjectArray.forEach((object) => {
+    //     newCollection.push(object)
+    // })
+    } else {
+        newCollection.push(vhs1, vhs2, vhs3)
+        return newCollection
+    }
+}
+
+function previewCollection(collectionArray) {
+    var collectionTitles = []
+    collectionArray.forEach((object) => {
+        collectionTitles.push(object.title)
+    })
+    return collectionTitles
+}
+
+module.exports = {
+    createTape,
+    reset,
+    createCollection,
+    previewCollection,
+  }
