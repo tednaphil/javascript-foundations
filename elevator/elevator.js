99c18b0573895fa7cf480d47a2631e04adb25a3e
Tayla Phillips <richardstayla@gmail.com>
2024-01-01 19:57:03 -0600

    Pass all tests

--- elevator/elevator.js
+++ elevator/elevator.js
@@ -1,3 +1,34 @@
+function createElevator(building, floors, currentFloor, passengers) {
+    return {
+        building,
+        floors,
+        currentFloor,
+        passengers,
+    }
+}
 
+function changeFloors(elevator, floor) {
+    if (floor === elevator.currentFloor) {
+        return `You're already on floor ${floor}!`
+    }
+    if (floor > elevator.floors) {
+        return `Floor ${floor} does not exist!`
+    } else {
+        elevator.currentFloor = floor
+        return `Taking you to floor ${elevator.currentFloor}!`
+    }
+}
 
-module.exports = {  };
+function dropOffPassenger(elevator, passenger) {
+    for (var i = 0; i < elevator.passengers.length; i++) {
+        if (passenger === elevator.passengers[i]) {
+            elevator.passengers.splice(i,1)
+        }
+    }
+    return elevator.passengers
+}
+module.exports = { 
+    createElevator,
+    changeFloors,
+    dropOffPassenger
+ };
